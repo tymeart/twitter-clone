8da663ab88a0858889e7334d563216e5e54d8ec1
Tiffany Lam <tlam5@sbcglobal.net>
2017-10-02 08:27:38 -0700

    Reorganize user data and tweet reactions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,23 +7,30 @@ class App extends Component {
     super(props);
     this.state = {
       user: {
-        givenName: 'Tiffany',
-        handle: 'tymeart',
-        avatar: '',
-        tweets: [{
-          id: 0,
-          content: 'I decided to make a Twitter clone in React, and one post can be divided into so many components! @o@',
-          comments: 1,
-          retweets: 0,
-          likes: 4
-        },
-        {
-          id: 1,
-          content: 'Going to see the Munch exhibit at SFMOMA today!',
-          comments: 0,
-          retweets: 0,
-          likes: 0
-        }]
+        tymeart: {
+          givenName: 'Tiffany',
+          avatar: {avatar},
+          tweets: [
+          {
+            id: 0,
+            content: 'I decided to make a Twitter clone in React, and one post can be divided into so many components! @o@',
+            reactions: {
+              comments: 1,
+              retweets: 0,
+              likes: 4
+            }
+          },
+          {
+            id: 1,
+            content: 'Going to see the Munch exhibit at SFMOMA today!',
+            reactions: {
+              comments: 0,
+              retweets: 0,
+              likes: 0
+            }
+          }
+          ]
+        }
       }
     }
   }
