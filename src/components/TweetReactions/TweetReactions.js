f605739359567b6784498c90b1f811e9524a62ad
Tiffany Lam <tlam5@sbcglobal.net>
2017-10-05 10:41:10 -0700

    Space out reaction items and render no number if there are 0 reactions

--- src/components/TweetReactions/TweetReactions.js
+++ src/components/TweetReactions/TweetReactions.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import FA from 'react-fontawesome';
+import './TweetReactions.css';
 
 const TweetReactions = (props) => {
+  const commentsNum = props.reactions.comments === 0 ? null : props.reactions.comments;
+  const retweetsNum = props.reactions.retweets === 0 ? null : props.reactions.retweets;
+  const likesNum = props.reactions.likes === 0 ? null : props.reactions.likes;
+
   return (
-    <div>
-      <FA name="comment-o"/><span>{props.reactions.comments}</span>
-      <FA name="retweet" /><span>{props.reactions.retweets}</span>
-      <FA name="heart-o" /><span>{props.reactions.likes}</span>
-      <FA name="bar-chart-o" />
+    <div className="TweetReactions">
+      <div><FA name="comment-o"/>{commentsNum}</div>
+      <div><FA name="retweet" />{retweetsNum}</div>
+      <div><FA name="heart-o" />{likesNum}</div>
+      <div><FA name="bar-chart-o" /></div>
     </div>
   );
 }
