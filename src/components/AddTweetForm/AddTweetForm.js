8cb083ee2d472b0a629c4821e37b12aaf675f3ed
Tiffany Lam <tlam5@sbcglobal.net>
2017-10-10 22:18:35 -0700

    Add icons at bottom of AddTweetForm

--- src/components/AddTweetForm/AddTweetForm.js
+++ src/components/AddTweetForm/AddTweetForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserAvatar from '../UserAvatar/UserAvatar';
+import FA from 'react-fontawesome';
 import './AddTweetForm.css';
 
 const AddTweetForm = (props) => {
@@ -13,9 +14,17 @@ const AddTweetForm = (props) => {
           value={props.tweetToAdd}
           onChange={props.handleInputChange}
         />
-        <div className="charCount-btn-group">
-          <div className="charCount">{props.charCount}</div>
-          <button onClick={props.handleAddTweet}>Tweet</button>
+        <div className="bottom-group">
+          <div className="bottom-group-icons">
+            <FA name="picture-o" />
+            <FA name="video-camera" />
+            <FA name="pie-chart" />
+            <FA name="map-marker" />
+          </div>
+          <div className="bottom-group-right">
+            <div className="charCount">{props.charCount}</div>
+            <button onClick={props.handleAddTweet}>Tweet</button>
+          </div>
         </div>
       </form>
     </div>
